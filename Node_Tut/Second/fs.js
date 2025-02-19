46c66394f921a313bf2c5ac56d0844127309eb46
MoushufAlam <rishu78698@gmail.com>
2025-02-20 00:36:05 +0530

    Still learning

---
+++ Node_Tut/Second/fs.js
@@ -0,0 +1,4 @@
+const fs = require('fs').writeFileSync
+fs("Hello.txt","Hello World")
+
+console.log("-->>", __dirname , "-->>" , __filename);
---
+++ Node_Tut/first/first.js
@@ -0,0 +1,4 @@
+var a=20
+var c=20
+var b=20
+console.log(a+b+c);
