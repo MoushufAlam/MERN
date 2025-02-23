152c0a4fc2c2b93eba5b91fab7c41c185cee24c1
MoushufAlam <rishu78698@gmail.com>
2025-02-23 18:48:17 +0530

    sa

---
+++ Node_Tut/Basic Api/data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    name: "Md Moushuf Alam",
+    email: "rishu78698@gmail.com",
+    college: "NIT Jamshedpur",
+  },
+  {
+    name: "Yadav Ji",
+    email: "yadavji@gmail.com",
+    college: "NIT Jamshedpur",
+  },
+];
+
+module.exports = data;
