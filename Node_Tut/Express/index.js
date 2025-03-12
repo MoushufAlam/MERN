0a69f1235d43893c6d3dc750a5b8305fe02d3158
MoushufAlam <rishu78698@gmail.com>
2025-03-12 21:53:28 +0530

    changed

--- Node_Tut/Express/index.js
+++ Node_Tut/Express/index.js
@@ -1,13 +1,36 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('',(req,resp)=>{
-    resp.send("Hello duniya");
-})
+app.get("", (req, resp) => {
+  resp.send(`
+    <h1>Welcome to my website!!!</h1>
+    <a href= "/about">Go to About Page</a>
+    </br>
+    <a href= "/help">Go to help Page</a>
+    `);
+});
 
-app.get('/about',(req,resp)=>{
-    resp.send("Hello World");
-})
+app.get("/about", (req, resp) => {
+  resp.send(`
+  <input type="text" placeholder = "Enter text"/>
+  <button>CLick me</button>
+  <a href= "/">Go to home Page</a>
+  </br>
+  <a href= "/help">Go to help Page</a>
+    `);
+});
+app.get("/help", (req, resp) => {
+  resp.send([
+    {
+      name: "ANil",
+      username: "wobble",
+    },
+    {
+      name: "Baston",
+      username: "gaglo",
+    },
+  ]);
+});
 
-app.listen(5500)
+app.listen(5500);
